eac3f4acc23f4f703b29a2899c57177c43d46373
gustavojimenezlievano <tavo0520@hotmail.com>
2024-04-12 21:50:43 -0500

    Add socket.io-client library for real-time communication

--- src/app/core/services/sensor/sensors.service.ts
+++ src/app/core/services/sensor/sensors.service.ts
@@ -4,16 +4,27 @@ import { SharedService } from '../../../shared/services/shared.service';
 import { Observable } from 'rxjs';
 import { SensorI } from '../../../core/models/Sensor';
 import { RecordSensorI } from '../../models/RecordSensor';
-
+import { io } from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SensorsService {
+  private socket: any = io('http://localhost:3000', {transports: ['websocket', 'polling', 'flashsocket']});
 
   constructor(
     private sharedService: SharedService
-  ) { }
+  ) {
+    console.log('SensorsService')
+
+    console.log('environment.baseUrl', environment.baseUrl);
+    // this.socket = io(environment.baseUrl);
+    this.test()
+   }
+
+  ngOnInit(): void {
+
+  }
 
   getAllSensors(): Observable<SensorI[]> {
     const url = `${environment.baseUrl}api/sensor/all`;
@@ -33,5 +44,30 @@ export class SensorsService {
   createSensorRecord(sensor: RecordSensorI): Observable<RecordSensorI> {
     const url = `${environment.baseUrl}api/record/sensor/create`;
     return this.sharedService.post<RecordSensorI>({ url: url, model: sensor });
+  }
+
+  getMessages() {
+    let observable = new Observable<any>(observer => {
+      this.socket.on('server:test01', (data: any) => {
+        observer.next(data);
+      });
+
+      this.socket.on('server:test02', (data: any) => {});
+
+      return () => { this.socket.disconnect(); };
+    });
+    return observable;
+  }
+
+  test() {
+    console.log('test');
+    this.socket.on('server:test01', (data: any) => {
+      console.log(data);
+    });
+
+
+
+    // this.socket.emit('client:test01', { my: 'data' });
+
   }
 }
